c578b419cebfb19137fc7f14ca8a79005f39f229
dcniccode <danielacadenavillanueva@gmail.com>
2024-06-13 11:29:18 -0500

    Se cambio el nombre

--- src/main/java/uni/isw/Application.java
+++ src/main/java/uni/isw/Application.java
@@ -10,16 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Application {
 
     public static void main(String[] args) {
-            SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
+
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer(){
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/**").allowedMethods("GET","POST","DELETE");
-            }            
-        };        
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE") ;
+            }
+        };
     }
 
 }
